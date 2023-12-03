39bfbd054bf72f92a146ac63b4a35bb6f1aeb21d
Alan Recalde <alanjulianrecalde@gmail.com>
2023-12-03 16:31:11 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,62 @@
+/* const massMark = 78;
+const heightMark = 1.69; // meters
+const massJohn = 92;
+const heightJohn = 1.95;
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn); 
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn);
+ */
+
+// Clase 17 Strings and templates literals
+
+/* const firstName = 'Alan';
+const job = 'Auditor';
+const age = 27;
+
+const alanNew = `i'm ${firstName}, a ${age} year old ${job}! `
+console.log(alanNew);
+
+console.log('Just a regular string');
+console.log(`string
+multiple 
+lines `) */
+
+// clase 28 - Taking decisions if/ els statemest
+
+/* const age = 14; 
+
+if(age >= 18){
+    console.log('Sarah can start driving license 🚚');
+} else{
+    const yearsLeft = 18 - age;
+console.log(`Sarah is too young. wait another ${yearsLeft} years 🙂`); 
+} */
+
+/* const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+}else {
+    century = 21;
+}
+console.log(century); */
+/* const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
+ */
+
+
